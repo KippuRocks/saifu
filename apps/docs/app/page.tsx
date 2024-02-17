dd66bd732f599c24d9557584feef3fadc8713e09
Pablo Andrés Dorado Suárez <hola@pablodorado.com>
2024-02-16 21:37:12 -0500

    chore(env): rename @repo org with @kippu

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
-import { Card } from "@repo/ui/card";
-import { Code } from "@repo/ui/code";
+import { Card } from "@kippu/ui/card";
+import { Code } from "@kippu/ui/code";
 import styles from "./page.module.css";
-import { Button } from "@repo/ui/button";
+import { Button } from "@kippu/ui/button";
 
 function Gradient({
   conic,
