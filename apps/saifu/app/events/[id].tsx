56e8b0b8523851a6962bf260868518dd4592b1c6
Pablo Andrés Dorado Suárez <hola@pablodorado.com>
2024-02-23 22:56:53 -0500

    fix: add reflect-metadata on layout

--- apps/saifu/app/events/[id].tsx
+++ apps/saifu/app/events/[id].tsx
@@ -2,11 +2,7 @@
 
 import "reflect-metadata";
 import Event from "../../components/Event/Event";
-import { useParams } from "next/navigation";
 
-export default function EventDetailPage() {
-  const params = useParams<{ id: string }>();
-  console.log(params);
-
-  return <Event id={Number(params.id)} />;
+export default function EventDetailPage({ id }: { id: number }) {
+  return <Event id={id} />;
 }
--- apps/saifu/app/layout.tsx
+++ apps/saifu/app/layout.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import "reflect-metadata";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
 import ThemeProvider from "@mui/material/styles/ThemeProvider";
 import theme from "./theme";
